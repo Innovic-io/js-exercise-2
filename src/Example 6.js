237c8179577af8b85c21cdf78697295111d0c4ea
Joco Stijakovic <jocostijakovic@gmail.com>
2018-11-09 16:17:40 +0100

    In this example, I used several methods, for example: method map, method push or pop and method slice.

---
+++ src/Example 6.js
@@ -0,0 +1,29 @@
+// Exmaple 6 //
+//
+let array1 = [1, 4, 9, 16];
+
+const newarray = array1.map(x => x * 2);
+
+console.log(newarray);
+
+let array2 = [3, 4, 7, 24];
+
+const newarray2= array2.map(x => x + x);
+
+console.log(newarray2);
+
+let array3 = [3, 5, 7, 32];
+
+console.log(array3.pop());
+
+array3.pop();
+
+console.log(array3);
+
+array3.push(3, 15);
+
+console.log(array3);
+
+array1.slice(0, 2);
+
+console.log(array1.slice(0, 2));
