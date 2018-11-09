4ea58b1c4f0e917f4e3a8c882643b7e73783c656
Joco Stijakovic <jocostijakovic@gmail.com>
2018-11-09 16:12:19 +0100

    Few exaples of If ELSE, finished.

---
+++ src/Example4.js
@@ -0,0 +1,43 @@
+// Example4 //
+
+let x;
+
+let time = 15;
+
+if (time < 10) {
+    x = "Good morning";
+}
+else if (time < 20) {
+    x = "Good day";
+}
+else {
+    x = "Good evening";
+}
+console.log(x);
+
+let t = 20;
+
+if (t > 21){
+    t = 'Not good'
+}
+else{
+    t = 'Its good!'
+}
+console.log(t);
+ let y = Func(4, 3);
+
+function Func(a, b) {
+    return a * b;
+}
+console.log(y);
+
+if (y < 21){
+    y = 'Not good'
+}
+else{
+    y = 'Its good!'
+}
+let z = 15;
+console.log( z < 21 ? "Not good" : "sada");
+
+console.log(y);
