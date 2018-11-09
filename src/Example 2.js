7f5983941cf88678ed3d87ca253f7f285200f9bc
Joco Stijakovic <jocostijakovic@gmail.com>
2018-11-09 15:26:05 +0100

    Multiply variables, adding variables, concatenating strings.

---
+++ src/Example 2.js
@@ -0,0 +1,24 @@
+// Exampl 2 //
+let b  = 6;
+
+let c;
+
+c = 15;
+
+console.log(b + c);
+
+let firstName = 'Joco';
+
+let lastName = 'Stijakovic';
+
+console.log(firstName + ' ' + lastName);
+
+let numberOfPhone1 = '066835';
+
+console.log(numberOfPhone1);
+
+let numberOfPhone2 = '581';
+
+console.log(numberOfPhone2);
+
+console.log(numberOfPhone1  + '' + numberOfPhone2);
