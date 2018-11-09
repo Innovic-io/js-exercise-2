fbb3553b1ce13bae17b16aca85ad2289d6780892
Joco Stijakovic <jocostijakovic@gmail.com>
2018-11-09 16:14:39 +0100

    Few examples with using Array.

---
+++ src/Example5.js
@@ -0,0 +1,33 @@
+// Example 5 //
+let numbers = [1, 2, 3];
+
+console.log(numbers);
+
+numbers[0] = 5;
+
+console.log(numbers);
+
+numbers[3] = 6;
+
+console.log(numbers);
+
+let last = numbers[numbers.length - 1];
+
+console.log(last);
+
+let person = [];
+
+person[0] = "Joco";
+
+person[1] = "Petar";
+
+person[2] = 46;
+
+person.push("Ivana");
+
+let x = person.length;
+
+let y = person[0];
+
+console.log(x);
+console.log(y);
