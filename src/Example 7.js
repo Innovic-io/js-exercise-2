9b81b84a3e69bc96936f186b1de3e480495684b6
Joco Stijakovic <jocostijakovic@gmail.com>
2018-11-12 10:35:59 +0100

    Loop through array of objects

--- src/Example 7.js
+++ src/Example 7.js
@@ -18,3 +18,38 @@ let string = ["J","o", "c", "o"];
 string.forEach(function (element) {
     console.log(element);
 })
+let http =['https','://', 'developer.mozilla.org', '/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/join'];
+
+console.log(http.join(''));
+
+let array = ['Marko is there', '" "', 'Pale'];
+
+let array2 = array.map(x => x.split(" "));
+
+console.log(array2);
+
+let months = ['Jan', 'March', 'April', 'June'];
+
+months.sort();
+
+console.log(months);
+
+months.splice(1,0, 'Feb');
+
+console.log(months);
+
+months.splice(4,0, 'May');
+
+console.log(months);
+
+console.log(months.sort());
+
+console.log(Array.from("Marko"));
+
+
+const result = array1.filter(word => word > 10);
+
+console.log(result);
+
+
+
