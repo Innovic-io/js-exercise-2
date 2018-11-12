bac43d87e2e9ef4d1dee054b8f1ed12125b0addf
Joco Stijakovic <jocostijakovic@gmail.com>
2018-11-12 13:22:21 +0100

    Loop through array of objects

--- src/Example 7.js
+++ src/Example 7.js
@@ -11,8 +11,6 @@ for (i = 0; i < cars.length; i++) {
 
 console.log(text);
 
-
-
 let string = ["J","o", "c", "o"];
 
 string.forEach(function (element) {
@@ -30,11 +28,14 @@ console.log(array2);
 
 let months = ['Jan', 'March', 'April', 'June'];
 
-months.sort();
+
 
 console.log(months);
-
-months.splice(1,0, 'Feb');
+/**
+ * The first number shows wich field we are star (we start from the other)
+ and the other shows whether we delete or not and (we delete 2 fields) and string shows what we can put on the array
+ */
+months.splice(1,2, 'Februar');
 
 console.log(months);
 
@@ -42,14 +43,21 @@ months.splice(4,0, 'May');
 
 console.log(months);
 
-console.log(months.sort());
 
-console.log(Array.from("Marko"));
+months.splice(6,0, 'September');
 
+console.log(months);
 
-const result = array1.filter(word => word > 10);
+/*
+ *filter elements that contain more than 5 letters
+ */
 
+const result = months.filter(word => word.length > 5);
 console.log(result);
 
 
+/*
+ *creates a new strings that are drawn from a given string
+ */
 
+console.log(Array.from("Marko"));
