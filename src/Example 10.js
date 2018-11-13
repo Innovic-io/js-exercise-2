dfe2f3ec097a33b11715ff85d094d3a26344c2c9
Joco Stijakovic <jocostijakovic@gmail.com>
2018-11-13 11:39:04 +0100

    Different between var - let - const

--- src/Example 10.js
+++ src/Example 10.js
@@ -4,7 +4,7 @@
 */
 
 let a = 10;
-
+// let a = 15 type let can't be re-defined
 a = 5;
 
 console.log(a);
@@ -13,55 +13,31 @@ console.log(a);
 */
 var b = 9;
 
+var b = 12
+
 b = 10;
 
 console.log(b);
 /*
-*This is error!
 * Type const cannot change its value
+*This is error!
 
 const c = 0;
-
+const c =  12 or
 c = 14;
 
 console.log(c);
 */
 
-/*
-* Type var can be accessible anywhere in function
-*/
-
-function f (age) {
-    if(age > 18) {
-        var status = 'adult';
-    }
-    console.log(status);
+for(var i = 0; i < 10; i++){
+    console.log(i);
 }
-f(20);
+console.log(i);
 
-/*
-* Type let cannot be accessible anywhere in function
-*  He is accesible outside the block
-*  Its not correct
-
-function h (age) {
-    if(age > 18) {
-        let status = 'adult';
-    }
-    console.log(status);
+//if it is declared inside the block, it will be not defined outside the block.
+for(let j = 0; j < 10; j++){
+    console.log(j);
 }
-h(45);
-*/
+//console.log(j);
 
-/*
-*  Its correct
-*/
 
-let status = 'adult';
-function h (age) {
-    if(age > 18) {
-
-    }
-    console.log(status);
-}
-h(45);
