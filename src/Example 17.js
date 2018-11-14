369456708dc764c1d56bb3002983ca11d7c51466
Joco Stijakovic <jocostijakovic@gmail.com>
2018-11-14 15:50:14 +0100

    Define strings and construct strings from other ones

---
+++ src/Example 17.js
@@ -0,0 +1,24 @@
+// Example 17
+
+const k = `"'`;
+
+const i = '"`';
+const s = "'`";
+
+console.log(s, k,  i);
+
+
+const par = 'sdadasd' +
+    'sdadasda' +
+    'dasdasda' +
+    'dssad' +  'dasdsas' + 'sdada';
+
+const par1 = `dsadas
+sdadasdas
+sadsads
+sdad  ${k} I can easly decorate my text ${i}`;
+
+
+console.log(par);
+
+console.log(par1);
