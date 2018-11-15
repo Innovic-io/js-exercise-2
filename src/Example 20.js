4ea19a7481d9504ca76e136d68c2771a80b4f9e6
Joco Stijakovic <jocostijakovic@gmail.com>
2018-11-15 17:12:26 +0100

    Few examples Promise function.

---
+++ src/Example 20.js
@@ -0,0 +1,39 @@
+// Example 20
+// TODO use arrow when possible
+
+function getNumberFunction () {
+    return Promise.resolve(4);
+}
+getNumberFunction().then(res => console.log(res));
+//
+// async - return Promise
+async function getNumber () {
+    return 4;
+}
+getNumber().then(res => console.log(res));
+
+// await operator
+
+async function f () {
+    let promise = new Promise((resolve) => {
+        setTimeout(() => resolve('Branislav'), 2000);
+    });
+    let res = await promise;
+    console.log(res);
+}
+
+f();
+function resolveAfter2Secund () {
+    return new Promise(resolve => {
+        setTimeout(() => resolve('Done'),1000)
+    });
+}
+
+async function asyncCall () {
+    console.log('calling');
+
+    const rest = await resolveAfter2Secund();
+
+    console.log(rest);
+}
+asyncCall();
