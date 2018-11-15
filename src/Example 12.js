88c835438f9b9c68b159e601fe594af663a959c2
Joco Stijakovic <jocostijakovic@gmail.com>
2018-11-15 17:11:52 +0100

    Few examples Promise function.

--- src/Example 12.js
+++ src/Example 12.js
@@ -1,2 +1,6 @@
-// Example 12 //
+// Example 12
 
+/*
+* In a synchronous programming model, things happen one at a time. When you call a function that performs a long-running action, it returns only when the action has finished and it can return the result.
+* This stops your program for the time the action takes.
+* An asynchronous model allows multiple things to happen at the same time. When you start an action, your program continues to run. When the action finishes, the program is informed and gets access to the result */
