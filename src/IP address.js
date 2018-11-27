f0c2e04023870adfb298c5aca3b7f383e7a4613f
Joco Stijakovic <jocostijakovic@gmail.com>
2018-11-27 13:40:28 +0100

    Several thing about IP address.

---
+++ src/IP address.js
@@ -0,0 +1,7 @@
+// IP address
+/*
+* What is IP address?
+ Every machine on a network has a unique identifier or IP(internet protocol)
+The IP address is similar to your phone number. Your phone number is a unique set of numbers that determines your phone so other people can call you.
+Similarly, an IP address is a unique set of numbers that determine your computer so that it can send and receive data from other computers.
+Currently, most IP addresses consist of four sets of numbers, separated by dots. 192.168.1.42 is an example of an IP address.*/
